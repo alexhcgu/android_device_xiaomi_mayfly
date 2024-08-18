e8394d2340f2951904d056b9877850d594e51555
MomoYuuki <2711300270@qq.com>
2024-08-19 02:07:33 +0800

    mayfly: Initial PixelOS

--- aosp_mayfly.mk
+++ aosp_mayfly.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from common aosp configuration
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from mayfly device
 $(call inherit-product, device/xiaomi/mayfly/device.mk)
@@ -18,7 +18,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := mayfly
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2206123SC
-PRODUCT_NAME := lineage_mayfly
+PRODUCT_NAME := aosp_mayfly
 
 PRODUCT_SYSTEM_NAME := mayfly
 PRODUCT_SYSTEM_DEVICE := mayfly
